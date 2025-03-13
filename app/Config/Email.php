bbf707fa667a89bdfd4858eee0d80dc1f4424102
hidayattresnadi <hidayattresnadi64@gmail.com>
2025-03-13 21:28:56 +0700

    Edit Email Credential

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = 'hidayattresnadi64@gmail.com';
-    public string $fromName   = 'Academic Management System';
+    public string $fromEmail  = '';
+    public string $fromName   = '';
     public string $recipients = '';
 
     /**
@@ -28,22 +28,22 @@ class Email extends BaseConfig
     /**
      * SMTP Server Hostname
      */
-    public string $SMTPHost = 'smtp.gmail.com';
+    public string $SMTPHost = '';
 
     /**
      * SMTP Username
      */
-    public string $SMTPUser = 'hidayattresnadi64@gmail.com';
+    public string $SMTPUser = '';
 
     /**
      * SMTP Password
      */
-    public string $SMTPPass = 'mres jukp xaxk yadz';
+    public string $SMTPPass = '';
 
     /**
      * SMTP Port
      */
-    public int $SMTPPort = 465;
+    public int $SMTPPort = 0;
 
     /**
      * SMTP Timeout (in seconds)
